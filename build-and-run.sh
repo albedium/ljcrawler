eb082dce59547bee86485732a3675452367b81f0
Anton Fedchenko <kompotik@gmail.com>
2011-05-23 18:44:36 +0400

    'gradle assemble' now produces working distribution

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,1 +1,2 @@
-gradle build && java -Xms512m -Xmx1024m -jar build/libs/ljcrawler-0.0.1.jar zyalt drugoi
+#gradle build && java -Xms512m -Xmx1024m -jar build/libs/ljcrawler-0.0.1.jar zyalt drugoi
+gradle assemble && cd build/distributions && unp *.zip && ./run.sh
---
+++ scripts/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar lib/ljcrawler-0.0.1.jar drugoi
